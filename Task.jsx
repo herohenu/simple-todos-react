41b4506d1e75e0da34ddc4c2df99cdc7f333b1b3
Yinghao Lei <leiyinghao.bj@gmail.com>
2015-07-22 16:54:05 +0800

    11.6  Add private button, shown only to owner

--- Task.jsx
+++ Task.jsx
@@ -3,7 +3,8 @@ Task = React.createClass({
     propTypes: {
         // This component gets the task to display through a React prop.
         // We can use propTypes to indicate it is required
-        task: React.PropTypes.object.isRequired
+        task: React.PropTypes.object.isRequired,
+        showPrivateButton: React.PropTypes.bool.isRequired
     },
     toggleChecked() {
         // Set the checked property to the opposite of its current value
@@ -14,6 +15,9 @@ Task = React.createClass({
       Meteor.call("removeTask", this.props.task._id);
     },
 
+    togglePrivate() {
+        Meteor.call("setPrivate", this.props.task._id, ! this.props.task.private);
+    },
     render() {
         //return (
         //    <li>{this.props.task.text}</li>
@@ -31,7 +35,13 @@ Task = React.createClass({
             checked={this.props.task.checked}
             onClick={this.toggleChecked} />
 
-            <span className="text">
+            { this.props.showPrivateButton ? (
+            <button className="toggle-private" onClick={this.togglePrivate}>
+            { this.props.task.private ? "Private" : "Public" }
+            </button>
+            ) : ''}
+
+<span className="text">
                 <strong>{this.props.task.username}</strong>: {this.props.task.text}
             </span>
 
